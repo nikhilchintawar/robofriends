f2ab281954f4bc1e8807d52b119f561ecad9b521
Nikhil Chintawar <chintawarnikhil88@gmail.com>
2020-04-21 09:34:00 +0530

    Updates

---
+++ precache-manifest.0ad12dac028f9f1d3877b25a535880f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8ceaf0b4596b136d08433c587a2a2cd",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "307088604bcf884e3b71",
+    "url": "/robofriends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "2e729e034faeb9dc712c",
+    "url": "/robofriends/static/css/main.4266ad29.chunk.css"
+  },
+  {
+    "revision": "307088604bcf884e3b71",
+    "url": "/robofriends/static/js/2.29d6aaa6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.29d6aaa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e729e034faeb9dc712c",
+    "url": "/robofriends/static/js/main.9f7f459b.chunk.js"
+  },
+  {
+    "revision": "8adbf24e3312ffc2ae3e",
+    "url": "/robofriends/static/js/runtime-main.403370f6.js"
+  }
+]);
